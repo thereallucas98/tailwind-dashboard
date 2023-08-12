12dc2c0d4447598c67161f58ca8f7603bed03e6b
thereallucas98 <lucas1998david@gmail.com>
2023-08-12 09:37:46 -0300

    finished sidebar

---
+++ src/components/Sidebar/UsedSpaceWidget.tsx
@@ -0,0 +1,44 @@
+import { Cog, LifeBuoy } from 'lucide-react'
+
+import { NavItem } from './NavItem'
+
+export function UseSpaceWidget() {
+  return (
+    <div className="mt-auto flex flex-col gap-6">
+      <nav className="space-y-0.5">
+        <NavItem title="Support" icon={LifeBuoy} />
+        <NavItem title="Settings" icon={Cog} />
+      </nav>
+
+      <div className="flex flex-col gap-4 rounded-lg bg-violet-50 px-4 py-5">
+        <div className="space-y-1">
+          <span className="text-sm/5 font-medium text-violet-700">
+            Used space
+          </span>
+          <p className="text-sm/5 text-violet-500">
+            Your team has used 80% of your available space. Need more?
+          </p>
+        </div>
+
+        <div className="h2 rounded-full bg-violet-100">
+          <div className="h-2 w-4/5 rounded-full bg-violet-600" />
+        </div>
+
+        <div className="space-x-3">
+          <button
+            type="button"
+            className="text-small font-medium text-violet-500 hover:text-violet-700"
+          >
+            Dismiss
+          </button>
+          <button
+            type="button"
+            className="text-small font-medium text-violet-700 hover:text-violet-900"
+          >
+            Upgrade plan
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,20 +1,43 @@
-import { Search } from 'lucide-react'
+import {
+  BarChart,
+  CheckSquare,
+  Flag,
+  Home,
+  Search,
+  SquareStack,
+  Users,
+} from 'lucide-react'
+
+import * as Input from '@/components/Input'
+
 import { Logo } from './Logo'
-import { MainNavigation } from './MainNavigation'
+import { NavItem } from './NavItem'
+import { UseSpaceWidget } from './UsedSpaceWidget'
+import { Profile } from './Profile'
 
 export function Sidebar() {
   return (
-    <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
+    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
-      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input
-          type="text"
-          placeholder="Search"
-          className="border-o flex-1 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
-        />
-      </div>
-      <MainNavigation />
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h5 w-5 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control placeholder="Search" />
+      </Input.Root>
+      <nav className="space-y-0.5">
+        <NavItem title="Home" icon={Home} />
+        <NavItem title="Dashboard" icon={BarChart} />
+        <NavItem title="Projects" icon={SquareStack} />
+        <NavItem title="Tasks" icon={CheckSquare} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={Users} />
+      </nav>
+
+      <UseSpaceWidget />
+
+      <div className="h-px bg-zinc-200" />
+      <Profile />
     </aside>
   )
 }
