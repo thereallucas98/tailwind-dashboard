b8d355dfba070639f4d215a3f1e28c0e1114ca6f
thereallucas98 <lucas1998david@gmail.com>
2023-08-12 13:36:05 -0300

    multiple file input

--- src/components/Form/FileInput/index.tsx
+++ src/components/Form/FileInput/index.tsx
@@ -1,6 +1,7 @@
 import { Control } from './Control'
 import { ImagePreview } from './ImagePreview'
 import { Root } from './Root'
+import { FileList } from './FileList'
 import { Trigger } from './Trigger'
 
-export { Control, ImagePreview, Root, Trigger }
+export { Control, ImagePreview, Root, FileList, Trigger }
