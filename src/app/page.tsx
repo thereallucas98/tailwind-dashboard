50743394ef56916b99b0663d516c44c4f0206358
thereallucas98 <lucas1998david@gmail.com>
2023-08-19 13:39:05 -0300

    variants & slots api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import { Bold, Italic, Link, List, ListOrdered, Mail } from 'lucide-react'
 
 import * as FileInput from '@/components/Form/FileInput'
 import * as Input from '@/components/Input'
+
+import { Button } from '@/components/Button'
 import { SettingsTabs } from '@/components/SettingsTabs'
 import { Select } from '@/components/Form/Select'
 import { SelectItem } from '@/components/Form/Select/SelectItem'
-import { Textarea } from '@/components/Form/TextArea'
+import { Textarea } from '@/components/Form/Textarea'
 
 export default function Home() {
   return (
@@ -23,19 +25,8 @@ export default function Home() {
             </span>
           </div>
           <div className="flex items-center gap-2">
-            <button
-              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-              type="button"
-            >
-              Cancel
-            </button>
-            <button
-              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
-              type="submit"
-              form="settings"
-            >
-              Save
-            </button>
+            <Button type="button" variant="outline">Cancel</Button>
+            <Button type="submit" form="settings" variant="primary">Save</Button>
           </div>
         </div>
 
@@ -154,39 +145,39 @@ export default function Home() {
                 </Select>
 
                 <div className="flex items-center gap-1">
-                  <button
+                  <Button
                     type="button"
-                    className="rounded-md p-2 hover:bg-zinc-50"
+                    variant="ghost"
                   >
                     <Bold className="h-4 w-4 text-zinc-500" strokeWidth={3} />
-                  </button>
-                  <button
+                  </Button>
+                  <Button
                     type="button"
-                    className="rounded-md p-2 hover:bg-zinc-50"
+                    variant="ghost"
                   >
                     <Italic className="h-4 w-4 text-zinc-500" strokeWidth={3} />
-                  </button>
-                  <button
+                  </Button>
+                  <Button
                     type="button"
-                    className="rounded-md p-2 hover:bg-zinc-50"
+                    variant="ghost"
                   >
                     <Link className="h-4 w-4 text-zinc-500" strokeWidth={3} />
-                  </button>
-                  <button
+                  </Button>
+                  <Button
                     type="button"
-                    className="rounded-md p-2 hover:bg-zinc-50"
+                    variant="ghost"
                   >
                     <List className="h-4 w-4 text-zinc-500" strokeWidth={3} />
-                  </button>
-                  <button
+                  </Button>
+                  <Button
                     type="button"
-                    className="rounded-md p-2 hover:bg-zinc-50"
+                    variant="ghost"
                   >
                     <ListOrdered
                       className="h-4 w-4 text-zinc-500"
                       strokeWidth={3}
                     />
-                  </button>
+                  </Button>
                 </div>
               </div>
 
